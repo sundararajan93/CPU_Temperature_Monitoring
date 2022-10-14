7b26323d02d9b42a42f12b3334235701655c478b
Sundararajan Thangaraj <geeksundar1993@gmail.com>
2022-10-14 22:54:16 +0530

    Changed sensor commands for accurate capture of temperature

--- 11_Sensor_value.py
+++ 11_Sensor_value.py
@@ -11,8 +11,8 @@ with open('sensor_data.csv', 'w') as file:
 for i in range(10000):
     with open('sensor_data.csv', 'a') as file:
         writer = csv.writer(file)
-        core_0 = os.popen("sensors -u | awk 'NR==4 {print $2}'").read()
-        core_1 = os.popen("sensors -u | awk 'NR==9 {print $2}'").read()
+        core_0 = os.popen("sensors -u | grep -A1 -i core | grep input| awk 'NR==1{print $2}'").read()
+        core_1 = os.popen("sensors -u | grep -A1 -i core | grep input| awk 'NR==2{print $2}'").read()
         time.sleep(1)
         print(i, core_0.strip(), core_1.strip())
         writer.writerow([i, core_0.strip(), core_1.strip()])
